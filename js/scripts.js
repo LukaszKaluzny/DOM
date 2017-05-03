3fc5d8dfbcf3cc2b332bb804c9ffe2b6062dee6a
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-03 20:11:11 +0200

    branch with master

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+console.log(document); // lub window.document
+
+window.onload = function() {
+  console.log('Strona w pełni załadowana.');
+  // reszta skryptu
+}
+var withMenuItemsClass = document.getElementsByClassName('button');
+
+console.log(withMenuItemsClass);
+
+for (var = withMenuItemsClass.length) {
+	button.innerText;
+}
