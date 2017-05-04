d8c1fd4a1d93540c329b3fd6d95ee3cbdc80a533
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-04 17:35:55 +0200

    branch with master

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,6 @@ var withMenuItemsClass = document.getElementsByClassName('button');
 
 console.log(withMenuItemsClass);
 
-for (withMenuItemsClass[0]; withMenuItemsClass[4]; withMenuItemsClass++) {
-	alert(withMenuItemsClass[0].innerText);
-	alert(withMenuItemsClass[1].innerText);
-	alert(withMenuItemsClass[2].innerText);
-	alert(withMenuItemsClass[3].innerText);
-	alert(withMenuItemsClass[4].innerText);
+for (var x = 0; x < withMenuItemsClass.length; x++) {
+	alert(withMenuItemsClass[x].innerText);
 }
